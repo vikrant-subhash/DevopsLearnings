720af8e8211c62c85023d0131527eb312efe6fa4
Vikrant <vikrant007yedave@gmail.com>
2022-08-10 03:19:06 +0530

    changes in VPC terraform

--- main.tf
+++ main.tf
@@ -18,12 +18,12 @@ resource "aws_vpc" "main" {
 }
 
 locals {
-    public_cidr = ["10.0.0.0/24", "10.0.1.0/24"]
-    private_cidr = ["10.0.2.0/24","10.0.3.0/24"]
+  public_cidr  = ["10.0.0.0/24", "10.0.1.0/24"]
+  private_cidr = ["10.0.2.0/24", "10.0.3.0/24"]
 }
 
 resource "aws_subnet" "public" {
- count=2
+  count = 2
 
   vpc_id     = aws_vpc.main.id
   cidr_block = local.public_cidr[count.index]
@@ -34,7 +34,7 @@ resource "aws_subnet" "public" {
 }
 
 resource "aws_subnet" "private" {
- count=2
+  count = 2
 
   vpc_id     = aws_vpc.main.id
   cidr_block = local.private_cidr[count.index]
@@ -54,13 +54,13 @@ resource "aws_internet_gateway" "main" {
 }
 
 resource "aws_eip" "nat" {
-    count = 2
+  count = 2
 
-  vpc      = true
+  vpc = true
 }
 
 resource "aws_nat_gateway" "main" {
-    count = 2
+  count         = 2
   allocation_id = aws_eip.nat[count.index].id
   subnet_id     = aws_subnet.private[count.index].id
 
@@ -84,16 +84,80 @@ resource "aws_route_table" "public" {
 
 resource "aws_route_table" "private" {
 
-    count = 2
+  count  = 2
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.main[count.index].id
   }
 
   tags = {
     Name = "private${count.index}"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count          = 2
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+resource "aws_route_table_association" "private" {
+  count          = 2
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private[count.index].id
+}
+
+resource "aws_security_group" "Public_SG" {
+  name        = "TF_PubSG"
+  description = "TF_PubSG"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "SSH from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+ }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "Public_SG"
+  }
+}
+
+
+resource "aws_security_group" "Private_SG" {
+  name        = "TF_PvtSG"
+  description = "TF_PvtSG"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "SSH from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["10.0.0.0/16"]
+ }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "Private_SG"
   }
 }
 
@@ -108,6 +172,6 @@ resource "tls_private_key" "rsa" {
 }
 
 resource "local_file" "TF" {
-    content  = tls_private_key.rsa.private_key_pem
-    filename = "TF_KEY"
+  content  = tls_private_key.rsa.private_key_pem
+  filename = "TF_KEY"
 }
