43862f623b03d2e2c512ceb96c3f801f7e508ef0
Vikrant <vikrant007yedave@gmail.com>
2022-08-10 03:32:28 +0530

    first terraform_code

--- main.tf
+++ main.tf
@@ -2,176 +2,22 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "~> 4.16"
     }
   }
+
+  required_version = ">= 1.2.0"
 }
 
-# Configure the AWS Provider
 provider "aws" {
   region = "ap-south-1"
 }
 
-# Create a VPC
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-}
-
-locals {
-  public_cidr  = ["10.0.0.0/24", "10.0.1.0/24"]
-  private_cidr = ["10.0.2.0/24", "10.0.3.0/24"]
-}
-
-resource "aws_subnet" "public" {
-  count = 2
-
-  vpc_id     = aws_vpc.main.id
-  cidr_block = local.public_cidr[count.index]
+resource "aws_instance" "main" {
+  ami           = "ami-05c8ca4485f8b138a"
+  instance_type = "t2.micro"
 
   tags = {
-    Name = "public${count.index}"
+    Name = "FirstterraformServer"
   }
-}
-
-resource "aws_subnet" "private" {
-  count = 2
-
-  vpc_id     = aws_vpc.main.id
-  cidr_block = local.private_cidr[count.index]
-
-  tags = {
-    Name = "private${count.index}"
-  }
-}
-
-
-resource "aws_internet_gateway" "main" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "main"
-  }
-}
-
-resource "aws_eip" "nat" {
-  count = 2
-
-  vpc = true
-}
-
-resource "aws_nat_gateway" "main" {
-  count         = 2
-  allocation_id = aws_eip.nat[count.index].id
-  subnet_id     = aws_subnet.private[count.index].id
-
-  tags = {
-    Name = "main"
-  }
-}
-
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main.id
-  }
-
-  tags = {
-    Name = "public"
-  }
-}
-
-resource "aws_route_table" "private" {
-
-  count  = 2
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.main[count.index].id
-  }
-
-  tags = {
-    Name = "private${count.index}"
-  }
-}
-
-resource "aws_route_table_association" "public" {
-  count          = 2
-  subnet_id      = aws_subnet.public[count.index].id
-  route_table_id = aws_route_table.public.id
-}
-resource "aws_route_table_association" "private" {
-  count          = 2
-  subnet_id      = aws_subnet.private[count.index].id
-  route_table_id = aws_route_table.private[count.index].id
-}
-
-resource "aws_security_group" "Public_SG" {
-  name        = "TF_PubSG"
-  description = "TF_PubSG"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    description      = "SSH from VPC"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
- }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name = "Public_SG"
-  }
-}
-
-
-resource "aws_security_group" "Private_SG" {
-  name        = "TF_PvtSG"
-  description = "TF_PvtSG"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    description      = "SSH from VPC"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["10.0.0.0/16"]
- }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name = "Private_SG"
-  }
-}
-
-resource "aws_key_pair" "TF" {
-  key_name   = "TF_KEY"
-  public_key = tls_private_key.rsa.public_key_openssh
-}
-
-resource "tls_private_key" "rsa" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
-resource "local_file" "TF" {
-  content  = tls_private_key.rsa.private_key_pem
-  filename = "TF_KEY"
 }
