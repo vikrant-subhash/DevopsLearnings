9b457332eb687c13ff6809313d8a38dcd10a32ae
Vikrant Yedave <vikrant007yedave@gmail.com>
2022-08-10 20:34:23 -0700

    changes in terraform_VPC

--- main.tf
+++ main.tf
@@ -161,17 +161,3 @@ resource "aws_security_group" "Private_SG" {
   }
 }
 
-resource "aws_key_pair" "TF" {
-  key_name   = "TF_KEY"
-  public_key = tls_private_key.rsa.public_key_openssh
-}
-
-resource "tls_private_key" "rsa" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
-resource "local_file" "TF" {
-  content  = tls_private_key.rsa.private_key_pem
-  filename = "TF_KEY"
-}
