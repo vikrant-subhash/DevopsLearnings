139ce23ef383fd1129ae35caf2e7b8a37ee913b2
Vikrant Yedave <vikrant007yedave@gmail.com>
2022-09-22 07:36:07 -0700

    Layered Approach

--- level1/outputs.tf
+++ level1/outputs.tf
@@ -1,15 +1,15 @@
 output "vpc_id" {
-    value = aws_vpc.main.id
+  value = aws_vpc.main.id
 }
 
 output "public_subnet_id" {
-    value = aws_subnet.public[*].id
+  value = aws_subnet.public[*].id
 }
 
 output "private_subnet_id" {
-    value = aws_subnet.private[*].id
+  value = aws_subnet.private[*].id
 }
 
 output "vpc_cidr" {
-    value = var.vpc_cidr
+  value = var.vpc_cidr
 }
--- level2/data.tf
+++ level2/data.tf
@@ -2,9 +2,9 @@ data "terraform_remote_state" "level1" {
   backend = "s3"
 
   config = {
-   bucket = "terraform-state-123bkp"
-   key = "level1.tfstate"
-   region = "ap-south-1"
-    }
+    bucket = "terraform-state-123bkp"
+    key    = "level1.tfstate"
+    region = "ap-south-1"
   }
+}
   
